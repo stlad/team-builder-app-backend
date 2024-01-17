5e85d7334bcd17f32339d17f2540f018524259ab
stlad <82213506+stlad@users.noreply.github.com>
2024-01-18 03:06:45 +0500

    init Belbin test microservice

---
+++ tba-mcs-belbin/src/main/java/ru/vaganov/tba/TbaBelbinApplication.java
@@ -0,0 +1,13 @@
+package ru.vaganov.tba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbaBelbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbaBelbinApplication.class, args);
+	}
+
+}
