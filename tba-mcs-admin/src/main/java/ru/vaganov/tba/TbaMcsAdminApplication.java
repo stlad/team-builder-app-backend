b39723eb6c8c2744e01ff23d411862efebc3aa43
stlad <82213506+stlad@users.noreply.github.com>
2024-03-20 16:07:06 +0500

    created tba-admin-mcs

---
+++ tba-mcs-admin/src/main/java/ru/vaganov/tba/TbaMcsAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.vaganov.tba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbaMcsAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbaMcsAdminApplication.class, args);
+	}
+
+}
