d7c15bd773e2a02cde3077057189a9a1f8160b86
stlad <82213506+stlad@users.noreply.github.com>
2024-03-25 16:46:57 +0500

    init rest client for tba-mcs-admin

---
+++ tba-mcs-rest-clients/tba-mcs-admin-api/src/main/java/ru/vaganov/tba/AdminApiClient.java
@@ -0,0 +1,45 @@
+package ru.vaganov.tba;
+
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import ru.vaganov.tba.dto.UserExternalDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@NoArgsConstructor
+public class AdminApiClient {
+
+    @Value("${admin.host}")
+    private String host;
+
+    @Value("${admin.context-path}")
+    private String contextPath;
+
+    public UserExternalDTO getUserById(Long id){
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        Map<String, String> vars = new HashMap<>();
+        vars.put("id", id.toString());
+
+        try {
+            var resp = restTemplate.exchange(host +contextPath+"/users/{id}",
+                    HttpMethod.GET,
+                    new HttpEntity<>(headers),
+                    UserExternalDTO.class,vars);
+
+            return resp.getBody();
+        }catch (Exception ex){
+            return  null;
+        }
+    }
+}
---
+++ tba-mcs-rest-clients/tba-mcs-admin-api/src/main/java/ru/vaganov/tba/dto/UserExternalDTO.java
@@ -0,0 +1,20 @@
+package ru.vaganov.tba.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class UserExternalDTO {
+    private Long id;
+    private String username;
+
+    private LocalDateTime dateRegister;
+    private String firstname;
+    private String lastname;
+    private String middlename;
+}
