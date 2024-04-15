0cb94eb806b240fc0788026439d5cab035294eba
stlad <82213506+stlad@users.noreply.github.com>
2024-04-15 12:22:34 +0500

    tba-mcs-hardskills api docs

--- tba-mcs-hardskills/src/main/java/ru/vaganov/tba/controllers/HardRolesController.java
+++ tba-mcs-hardskills/src/main/java/ru/vaganov/tba/controllers/HardRolesController.java
@@ -1,6 +1,7 @@
 package ru.vaganov.tba.controllers;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +23,20 @@ public class HardRolesController {
     @Autowired
     private HardRolesService hardRolesService;
 
+
+    @Operation(summary = "Получение всех проф. ролей", description = "Получение всего каталога проф. ролей")
     @GetMapping("/all")
     public ResponseEntity<List<HardRoleDTO>> findAllRoles(){
         return new ResponseEntity<>(hardRolesService.findAllRoles(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Получение проф. роли по её ID", description = "Получение проф. роли по её ID")
     @GetMapping("/id/{id}")
     public ResponseEntity<HardRoleDTO> findRoleById(@PathVariable Long id){
         return new ResponseEntity<>(hardRolesService.findRoleById(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Получение проф. роли по её имени", description = "Получение проф. роли по её имени")
     @GetMapping("/{roleName}")
     public ResponseEntity<HardRoleDTO> findRoleByName(@PathVariable String roleName){
         return new ResponseEntity<>(hardRolesService.findRoleByName(roleName), HttpStatus.OK);
--- tba-mcs-hardskills/src/main/java/ru/vaganov/tba/controllers/ResultController.java
+++ tba-mcs-hardskills/src/main/java/ru/vaganov/tba/controllers/ResultController.java
@@ -1,5 +1,6 @@
 package ru.vaganov.tba.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,21 +27,26 @@ public class ResultController {
     @Autowired
     private ResultsService resultsService;
 
+
+    @Operation(summary = "Получение впроф. роли и сколько осталось не нее мест")
     @GetMapping("/catalog")
     public ResponseEntity<List<HardRoleWithQuotaDTO>>getRolesWithQuota(){
         return new ResponseEntity<>(resultsService.getCatalogWithQuotas(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Сохранение результата выбора роли")
     @PostMapping("/")
     public ResponseEntity<RoleResultDTO> saveResult(@RequestBody RoleResultShortDTO dto){
         return new ResponseEntity<>(resultsService.saveResult(dto), HttpStatus.OK);
     }
 
+    @Operation(summary = "Получение результата выбора по ID пользователя")
     @GetMapping("/user/{id}")
     public ResponseEntity<RoleResultDTO> findByUserId(@PathVariable Long id){
         return new ResponseEntity<>(resultsService.findByUserId(id),HttpStatus.OK);
     }
 
+    @Operation(summary = "Получение всего списка результатов")
     @GetMapping("/")
     public ResponseEntity<List<RoleResultShortDTO>> getAllResults(){
         return new ResponseEntity<>(resultsService.getAllUserResults(), HttpStatus.OK);
